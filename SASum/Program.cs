00a38414f40e48e977ed653628dbda9c7f945fcc
nilfoj <nilfoj@hotmail.com>
2024-02-17 14:05:42 -0300

    Initial commit

---
+++ SASum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HackerRank
+{
+    class Result
+    {
+        public static int simpleArraySum(List<int> ar)
+        {
+     
+            int sum = 0;
+            foreach (var item in ar)
+            {
+                sum += item;
+            }
+            
+            return sum;
+        }
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            
+            int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+            int result = Result.simpleArraySum(ar);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ SMFirst/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HackerRank
+{
+    class SMFirst
+    {
+
+        static int solveMeFirst(int a, int b)
+        {
+            return a + b;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Console.Clear();
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            int sum = solveMeFirst(val1, val2);
+            
+            Console.Write(sum);
+        }
+    }
+}
