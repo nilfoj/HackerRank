d648fbeed7b244c35a428f0fd1980f4645f451b3
nilfoj <nilfoj@hotmail.com>
2024-03-06 22:30:26 -0300

    HackerRank_CTTriples

---
+++ DDifference/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HackerRank
+{
+    //    class Result
+    //    {
+    //        public static int diagonalDifference(List<List<int>> arr)
+    //        {
+
+
+    //        }
+    //    }
+
+    //    class Solution
+    //    {
+    //        public static void Main(string[] args)
+    //        {
+
+    //            int n  = Convert.ToInt32(Console.ReadLine().Trim());
+
+    //            List<List<int> arr = new List<List<int>>();
+
+    //            for (int i = 0; i < n; i++)
+    //            {
+
+    //                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+
+    //                int result = Result.diagonalDifference(arr);
+
+    //            }
+
+    //        }
+    //    }
+}
+
+
+
